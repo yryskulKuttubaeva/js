3cc1990094ad916680f8d58e6c5c4ea597a9a480
yryskul <kuttubaevayryskul@gmail.com>
2021-12-24 15:19:48 +0600

    while, for

--- script.js
+++ script.js
@@ -18,6 +18,55 @@
 // var example1 = "Hello" + "World";
 // console.log(example1);
 
-var age = prompt("How old are you?")
-var year = 2020 - age;
-console.log(year);
+// var age = prompt("How old are you?")
+// var year = 2020 - age;
+// console.log(year);
+
+// var year = prompt("What year Kyrgyzstan gained inderendence?");
+// if (year > 1991){
+//     alert("This you is after independebce year");
+// }
+// else if (year ==="1991"){
+//     alert("You got it!");
+// }
+// else {
+//     alert("This year is before independence year");
+// }
+
+// var city = prompt("город");
+// if (city === "Каракол"){
+//     alert("Иссик-куль");
+// }
+// else if  (city === "Бишкек"){
+//      alert("Чуйская область");
+// }
+
+// else if  (city === "Ош"){
+//    alert("Ошская область");
+// }
+// else if  (city === "Жалал-Абад"){
+//     alert("Жалал-Абадская область");
+//  }
+//  else if  (city === "Нарын"){
+//     alert("Нарынская область");
+//  }
+//  else if  (city === "Баткен"){
+//     alert("Баткенская область");
+//  }
+//  else if  (city === "Талас"){
+//     alert("Талаская область");
+//  }
+
+
+// var currrentNumber = 0;
+// var stopNumber = 150;
+
+// while (currrentNumber > stopNumber  ){
+//     currrentNumber = stopNumber - 1;
+//     console.log(stopNumber)
+// }
+
+ for (var i=1; i<=10; i++){
+     console.log(i);
+ }
+
