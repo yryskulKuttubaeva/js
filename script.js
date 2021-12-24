6847998ccb81b9d22a5209b8ba76a7a389b31244
yryskul <kuttubaevayryskul@gmail.com>
2021-12-24 14:23:44 +0600

    add js

---
+++ script.js
@@ -0,0 +1,19 @@
+// var message = ptompt("What is your message?", "no mesaage");
+// if (message === ""){
+//     message = "no message";
+// }
+// alert(message);
+
+// var year = prompt("What year Kyrgyzstan gainer independence?");
+// if(year === "1991");{
+//     alert("Correct!")
+// }
+// else {
+//     alert("Incorrect");
+// }
+
+var example1 = 100 + 100;
+console.log(example1);
+
+var example1 = "Hello" + "World";
+console.log(example1)
